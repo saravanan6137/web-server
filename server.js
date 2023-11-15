a7175b27e48a33dad352ddf987b5117139d3ab3b
Saravanan-Kannan <saravanan.kannan@maybank.com>
2023-11-15 15:28:28 +0800

    fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 3000;
 
 const generateAASA = () => {
     return {
