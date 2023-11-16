a6c21f3ddfeb6f908fd7a971191b3ac4594cf3b2
Saravanan-Kannan <saravanan.kannan@maybank.com>
2023-11-16 14:23:28 +0800

    8

--- SmartBanner.js
+++ SmartBanner.js
@@ -1,16 +1,82 @@
-const getSmartBanner = (isIOS) => {
-  if (isIOS) {
-    // iOS Smart App Banner
+const getBanner = (appName, appDescription, appIcon, installLink) => {
     return `
-      <meta name="apple-itunes-app" content="app-id=1581543726">
-      `;
-  } else {
-    // Android Smart App Banner
-    return `
-      <meta name="apple-itunes-app" content="app-id=1581543726">
-      `;
-  }
-  // Include other meta tags as needed
+    <style>
+      /* Styles for the banner component */
+      @keyframes slideIn {
+        from {
+          transform: translateY(-100%);
+        }
+        to {
+          transform: translateY(0);
+        }
+      }
+
+      .banner {
+        background-color: #f5f5f5;
+        padding: 20px;
+        text-align: center;
+        border-radius: 15px; /* Adjust the radius as needed */
+        margin: 10px; /* Add a 10px margin on all sides */
+        animation: slideIn 0.5s ease-out; /* Animation definition */
+      }
+
+      .banner-content {
+        max-width: 600px;
+        margin: 0 auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+        margin-right: 20px;
+      }
+
+      .text-content {
+        text-align: left;
+      }
+
+      h2 {
+        font-size: 1.5em;
+        margin-bottom: 10px;
+      }
+
+      p {
+        color: #666;
+        margin-bottom: 15px;
+      }
+
+      .install-button {
+        display: inline-block;
+        padding: 10px 20px;
+        background-color: #007BFF;
+        color: #fff;
+        text-decoration: none;
+        border-radius: 5px;
+        font-weight: bold;
+        transition: background-color 0.3s ease;
+      }
+
+      .install-button:hover {
+        background-color: #0056b3;
+      }
+    </style>
+
+    <!-- Banner Section -->
+    <section class="banner">
+      <div class="banner-content">
+        <img src="${appIcon}" alt="App Icon">
+        <div class="text-content">
+          <h2>${appName}</h2>
+          <p>${appDescription}</p>
+          <a href="${installLink}" class="install-button" target="_blank">Install Now</a>
+        </div>
+      </div>
+    </section>
+  `;
 };
-// Export the function directly
-module.exports = getSmartBanner;
+
+module.exports = getBanner;
--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
-// const getSmartBanner = require('./SmartBanner');
-
+const getBanner = require("./SmartBanner");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -30,19 +29,28 @@ app.get("/.well-known/apple-app-site-association", (req, res) => {
 
 // Serve the /dashboard route
 app.get("/dashboard", (req, res) => {
-  const userAgent = req.headers['user-agent'];
+  const userAgent = req.headers["user-agent"];
   const isIOS = /iPhone|iPad/.test(userAgent);
-  console.log('jbdaj', isIOS)
+  console.log("jbdaj", isIOS);
+  const banner = getBanner(
+    "MyAhb",
+    "The myAHB Mobile App is the official mobile app by Maybank Asset Management Sdn Bhd, who is fully owned by Maybank Asset Management Group. The mobile app allows its users to safely and conveniently access and view their AHB investment portfolio all in one app on their mobile/tablet devices. The app is only applicable to Maybank AHB account holders.",
+    "app-icon.png",
+    "https://apple.co/47zd5Xj"
+  );
   // Send the dashboard with the smart banner
   res.send(`
-    <html>
-      <head>
-      <meta name="apple-itunes-app" content="app-id=1581543726">
-      </head>
-      <body>
-        <h1>Welcome to the dashboard!</h1>
-      </body>
-    </html>
+  <html>
+  <head>
+    <title>Your App Name</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    ${banner}
+    <h1>Welcome to the dashboard!</h1>
+    <!-- Other dashboard content goes here -->
+  </body>
+</html>
   `);
 });
 
