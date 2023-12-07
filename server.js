751f8f244bd3a9f6137419b4b2e2dfd358f5d288
saravanan6137 <saravanan.kannan@maybank.com>
2023-12-07 14:11:38 +0800

    fixed aasa

--- server.js
+++ server.js
@@ -16,23 +16,7 @@ const generateAASA = () => {
           appID: `7PGNU69TNW.com.mamsb.myahb.innov`,
           components: [
             {
-              "/": "/dashboard/*",
-
-              comment: "Matches any URL whose path starts with /dashboard/",
-            },
-            {
-              "/": "/investmentTransaction/*",
-
-              comment:
-                "Matches any URL whose path starts with /investmentTransaction/",
-            },
-            {
-              "/": "/help/*",
-
-              "?": { articleNumber: "????" },
-
-              comment:
-                "Matches any URL whose path starts with /help/ and which has a query item with name 'articleNumber' and a value of exactly 4 characters",
+              "/": "*"
             },
           ],
         },
@@ -53,42 +37,26 @@ app.get("/.well-known/assetlinks.json", (req, res) => {
 });
 
 // Serve the /dashboard route
-app.get("/dashboard", (req, res) => {
+app.get("/scanandpay", (req, res) => {
   const userAgent = req.headers["user-agent"];
   const isIOS = /iPhone|iPad/.test(userAgent);
-  console.log("jbdaj", isIOS);
-  const banner = getBanner(
-    "MyAhb",
-    "The myAHB Mobile App is the official mobile app by Maybank Asset Management Sdn Bhd, who is fully owned by Maybank Asset Management Group. The mobile app allows its users to safely and conveniently access and view their AHB investment portfolio all in one app on their mobile/tablet devices. The app is only applicable to Maybank AHB account holders.",
-    "app-icon.png",
-    "https://apple.co/47zd5Xj"
-  );
-  // Send the dashboard with the smart banner
-  res.send(`
-  <html>
-  <head>
-    <title>Your App Name</title>
-    <link rel="stylesheet" href="styles.css">
-  </head>
-  <body>
-    ${banner}
-    <h1>Welcome to the dashboard!</h1>
-    <!-- Other dashboard content goes here -->
-  </body>
-</html>
-  `);
-});
+  let banner;
+  if (isIOS) {
+    banner = getBanner(
+      "MAE UAT",
+      "The myAHB Mobile App is the official mobile app by Maybank Asset Management Sdn Bhd, who is fully owned by Maybank Asset Management Group. The mobile app allows its users to safely and conveniently access and view their AHB investment portfolio all in one app on their mobile/tablet devices. The app is only applicable to Maybank AHB account holders.",
+      "app-icon.png",
+      "https://apple.co/47zd5Xj"
+    );
+  } else {
+    banner = getBanner(
+      "MAE UAT",
+      "The MAE app has all your M2U banking features and beyond. Apart from viewing, tracking, and managing your finances, the MAE app helps you manage tasks and plan your lifestyle with just a few taps.",
+      "app-icon.png",
+      "https://play.google.com/store/apps/details?id=com.maybank2u.life&hl=en-MY"
+    );
+  }
 
-app.get("/help", (req, res) => {
-  const userAgent = req.headers["user-agent"];
-  const isIOS = /iPhone|iPad/.test(userAgent);
-  console.log("jbdaj", isIOS);
-  const banner = getBanner(
-    "MyAhb",
-    "The myAHB Mobile App is the official mobile app by Maybank Asset Management Sdn Bhd, who is fully owned by Maybank Asset Management Group. The mobile app allows its users to safely and conveniently access and view their AHB investment portfolio all in one app on their mobile/tablet devices. The app is only applicable to Maybank AHB account holders.",
-    "app-icon.png",
-    "https://apple.co/47zd5Xj"
-  );
   // Send the dashboard with the smart banner
   res.send(`
     <html>
@@ -98,7 +66,43 @@ app.get("/help", (req, res) => {
     </head>
     <body>
       ${banner}
-      <h1>Welcome to the help!</h1>
+      <h1>Welcome to the Scan and Pay!</h1>
+      <!-- Other dashboard content goes here -->
+    </body>
+  </html>
+    `);
+});
+
+app.get("/createtabung", (req, res) => {
+  const userAgent = req.headers["user-agent"];
+  const isIOS = /iPhone|iPad/.test(userAgent);
+  let banner;
+  if (isIOS) {
+    banner = getBanner(
+      "MAE UAT",
+      "The myAHB Mobile App is the official mobile app by Maybank Asset Management Sdn Bhd, who is fully owned by Maybank Asset Management Group. The mobile app allows its users to safely and conveniently access and view their AHB investment portfolio all in one app on their mobile/tablet devices. The app is only applicable to Maybank AHB account holders.",
+      "app-icon.png",
+      "https://apple.co/47zd5Xj"
+    );
+  } else {
+    banner = getBanner(
+      "MAE UAT",
+      "The MAE app has all your M2U banking features and beyond. Apart from viewing, tracking, and managing your finances, the MAE app helps you manage tasks and plan your lifestyle with just a few taps.",
+      "app-icon.png",
+      "https://play.google.com/store/apps/details?id=com.maybank2u.life&hl=en-MY"
+    );
+  }
+
+  // Send the dashboard with the smart banner
+  res.send(`
+    <html>
+    <head>
+      <title>Your App Name</title>
+      <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+      ${banner}
+      <h1>Welcome to the Tabung!</h1>
       <!-- Other dashboard content goes here -->
     </body>
   </html>
